a5d68cc3abf7408ead1aa0c20c1d9f21205ee069
Djavan Cackley <djavancackley@Djavans-MBP.home>
2019-03-14 19:42:08 -1000

    created knex, docker-compose yml, .env, dockerfile

--- server.js
+++ server.js
@@ -1,2 +1,27 @@
-// TESTINGGGGG
-//TESTING WERRRRRKS!
+const express = require('express');
+const bodyParser = require('body-parser');
+const User = require('./database/models/User');
+
+// data vars
+const PORT = process.env.PORT;
+const SESSION_SECRET = process.env.SESSION_SECRET;
+const REDIS_HOSTNAME = process.env.REDIS_HOSTNAME;
+
+if (!PORT) { console.log('No Port Found'); }
+if (!SESSION_SECRET) { console.log('No Session Secret Found'); }
+if (!REDIS_HOSTNAME) { console.log('No Redis Hostname Found'); }
+if (!PORT || !SESSION_SECRET || !REDIS_HOSTNAME) { return process.exit(1); }
+
+// setup server middleware
+const app = express();
+app.use(bodyParser.json({ extended: true }));
+
+// routes
+app.get('/api/smoke', (req, res) => {
+    res.json({ smoke: 'test' });
+});
+
+// start server
+app.listen(PORT, () => {
+    console.log(`Server stated on port: ${PORT}`);
+});
