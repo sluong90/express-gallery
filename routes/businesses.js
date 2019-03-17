016edb5ec10281bf205ea5f466504b6d57d9e484
Sylvia <syll321.sl@gmail.com>
2019-03-17 12:26:30 -1000

    get/post works

--- routes/businesses.js
+++ routes/businesses.js
@@ -1,39 +1,20 @@
-<<<<<<< HEAD
-// const express = require('express');
-// const router = express.Router();
-// const knex = require('../database');
-
-// router.get('/', function (req, res) {
-//     knex.select('*').from('business')
-//         .then((business) => {
-//             console.log(business);
-//             res.render('business', { business })
-//         });
-
-// })
-
-// module.exports = router;
-=======
 const express = require('express');
 const router = express.Router();
+const knex = require('../database');
 const Business = require('./database/models/Business');
 
 
-router.get('/', function (req, res) {
-    return new Business().fetchAll()
-    .then((business) => {
-      return res.json(business);
-    })
-    .catch((err) => {
-      console.log(err);
-      res.sendStatus(500);
-    });
-});
+router.get('/businesses', (req, res) => {
 
-router.get('/new', function (req, res) {
-    res.render('home')
+  return new Business()
+      .fetchAll()
+      .then(business => {
+          return res.json(business);
+      })
+      .catch((err) => {
+          console.log(err);
+          res.sendStatus(500);
+      });
 });
-
 
 module.exports = router;
->>>>>>> cdc8385fb40357215bfdebf84d51337739df9adc
--- server.js
+++ server.js
@@ -32,30 +32,20 @@ app.get('/', (req, res) => {
     res.render("homebase")
 });
 
-<<<<<<< HEAD
 
-// router.get('/', function (req, res) {
-//     knex.select('*').from('business')
-//         .then((business) => {
-//             console.log(business);
-//             res.render('business', { business })
+
+// app.get('/businesses', (req, res) => {
+
+//     return new Business()
+//         .fetchAll()
+//         .then(business => {
+//             return res.json(business);
+//         })
+//         .catch((err) => {
+//             console.log(err);
+//             res.sendStatus(500);
 //         });
-
-// })
-
-app.get('/businesses', (req, res) => {
-    // res.json({ fuck: 'fuck' })
-
-    return new Business()
-        .fetchAll()
-        .then(business => {
-            return res.json(business);
-        })
-        .catch((err) => {
-            console.log(err);
-            res.sendStatus(500);
-        });
-});
+// });
 
 // app.post('/api/users', (req, res) => {
 //     const username = req.body.username;
@@ -68,8 +58,6 @@ app.get('/businesses', (req, res) => {
 //             res.sendStatus(500);
 //         });
 // })
-=======
->>>>>>> cdc8385fb40357215bfdebf84d51337739df9adc
 
 // start server
 app.listen(PORT, () => {
