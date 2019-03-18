df5f285ee05b89996e99816830761363f550fcaa
Sylvia <syll321.sl@gmail.com>
2019-03-17 18:19:07 -1000

    delete works on postman, edit in progress

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const Business = require('./database/models/Business');
 const hbs = require('express-handlebars');
+const methodOverride = require('method-override');
 
 
 
@@ -19,6 +20,7 @@ if (!PORT || !SESSION_SECRET || !REDIS_HOSTNAME) { return process.exit(1); }
 const app = express();
 app.use(bodyParser.json({ extended: true }));
 app.use(bodyParser.json())
+app.use(methodOverride('_method'))
 app.engine('hbs', hbs({
     defaultLayout: 'home',
     extname: '.hbs'
@@ -45,10 +47,14 @@ app.get('/businesses', (req, res) => {
         });
 });
 
+app.get('/businesses/new', (req,res) => {
+    res.render('templates/new');
+})
+
 app.post('/businesses', (req, res) => {
     const {author, url, description} = req.body;
     return new Business({ author, url, description }).save()
-        .then((business) => {
+        .then(() => {
             return res.redirect('/businesses');
         })
         .catch((err) => {
@@ -71,8 +77,32 @@ app.get('/businesses/:id', (req, res) => {
     })
 })
 
-app.get('businesses/new', (req,res) => {
-    res.render('templates/new');
+
+app.get('/businesses/:id/edit', (req, res) => {
+    const id = Number(req.params.id);
+    return new Business()
+    .where({id})
+    .fetchAll()
+    .then(update => {
+        return res.render('templates/edit', update.toJSON());
+    })
+    .catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    })
+})
+
+app.delete('/businesses/:id', (req,res) => {
+    return new Business()
+    .where({id: req.params.id})
+    .destroy()
+    .then(result => {
+        return res.redirect('/businesses');
+    })
+    .catch((err) => {
+        console.log(err);
+        res.sendStatus(500);
+    })
 })
 
 // start server
